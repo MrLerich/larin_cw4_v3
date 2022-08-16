acaaf6faa89672c4de957edefefe53a42de4af1a
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-08-16 23:17:57 +0300

    add models

--- project/models.py
+++ project/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String
+from marshmallow.fields import Integer
+from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 from project.setup.db import models
 
@@ -6,4 +8,34 @@ from project.setup.db import models
 class Genre(models.Base):
     __tablename__ = 'genres'
 
-    name = Column(String(100), unique=True, nullable=False)
+    name = Column(String(255), unique=True, nullable=False)
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(255), unique=True, nullable=False)
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    title = Column(String(255))
+    description = Column(String(255))
+    trailer = Column(String(255))
+    year = Column(Integer())
+    rating = Column(Float())
+    genre_id = Column(Integer(), ForeignKey(f'{Genre.__tablename__}.id'))
+    director_id = Column(Integer(), ForeignKey(f'{Director.__tablename__}.id'))
+
+    genre = relationship(Genre)
+    director = relationship(Director)
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    name = Column(String(255))
+    surname = Column(String())
+    favorite_genre = Column(Integer(), ForeignKey(f'{Genre.__tablename__}.id'))
+
+    genre = relationship(Genre)
