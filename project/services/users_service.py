38abd2d6adce5852316a5350042e8c813f53d5ae
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-08-17 13:35:15 +0300

    fix user_service

--- project/services/users_service.py
+++ project/services/users_service.py
@@ -2,17 +2,17 @@ from typing import Optional
 
 from project.dao.base import BaseDAO
 from project.exceptions import ItemNotFound
-from project.models import Genre
+from project.models import User
 
 
-class GenresService:
+class UsersService:
     def __init__(self, dao: BaseDAO) -> None:
         self.dao = dao
 
-    def get_item(self, pk: int) -> Genre:
-        if genre := self.dao.get_by_id(pk):
-            return genre
-        raise ItemNotFound(f'Genre with pk={pk} not exists.')
+    def get_item(self, pk: int) -> User:
+        if user := self.dao.get_by_id(pk):
+            return user
+        raise ItemNotFound(f'User with pk={pk} not exists.')
 
-    def get_all(self, page: Optional[int] = None) -> list[Genre]:
+    def get_all(self, page: Optional[int] = None) -> list[User]:
         return self.dao.get_all(page=page)
